987c49fa3c296f453dcbd16d2260dfeb55d1856e
Jose David Pacheco Valedo <josepdark@gmail.com>
2017-05-29 17:55:43 -0500

    add css files

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'my-app',
+    selector: 'body',
     templateUrl: 'app/app.component.html'
 })
 export class AppComponent { }
