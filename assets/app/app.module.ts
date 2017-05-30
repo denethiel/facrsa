7c8cee160a26eb683e5378327354ba1d2d0215bb
Jose David Pacheco Valedo <josepdark@gmail.com>
2017-05-30 18:07:37 -0500

    add ngx-bootstrap

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule }       from '@angular/core';
 import { BrowserModule  } from '@angular/platform-browser';
 import { AppComponent }   from './app.component';
+import { ModalModule } from 'ngx-bootstrap';
 
 @NgModule({
     declarations: [AppComponent],
-    imports:      [BrowserModule],
+    imports:      [BrowserModule, ModalModule.forRoot()],
     bootstrap:    [AppComponent],
 })
 export class AppModule {}
