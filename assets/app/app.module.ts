59cc5ef2a23bb31f947a8c68b9ff072b77493348
Jose David Pacheco Valedo <josepdark@gmail.com>
2017-06-07 18:08:59 -0500

    add mobile menu

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule  } from '@angular/platform-browser';
 import { AppComponent }   from './app.component';
 import { ModalModule, BsDropdownModule } from 'ngx-bootstrap';
 import { NAV_DROPDOWN_DIRECTIVES } from './shared/nav-dropdown.directive';
+import { NAV_MENU_DIRECTIVES } from './shared/nav-menu.directive';
 
 @NgModule({
     declarations: [
         AppComponent,
-        NAV_DROPDOWN_DIRECTIVES ],
+        NAV_DROPDOWN_DIRECTIVES,
+        NAV_MENU_DIRECTIVES ],
     imports:      [BrowserModule, ModalModule.forRoot(), BsDropdownModule.forRoot()],
     bootstrap:    [AppComponent],
 })
