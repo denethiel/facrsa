50bc73e05b111d2cb3f20665baaaf28d71fcf530
Jose David Pacheco Valedo <josepdark@gmail.com>
2017-06-14 18:16:09 -0500

    login validator

--- assets/app/pages/login.component.ts
+++ assets/app/pages/login.component.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
+import { FormBuilder,
+         FormGroup,
+         Validators} from '@angular/forms';
 
 @Component({
     templateUrl:'app/pages/login.component.html'
 })
 export class LoginComponent{
-    constructor() { }
+    loginForm: FormGroup;
+    constructor(fb: FormBuilder) {
+        this.loginForm = fb.group({
+            'email':['', Validators.compose([Validators.required, Validators.email]) ],
+            'password':['',Validators.required]
+        })
+     }
+     
+     onsubmit(value:any):void{
+         console.log('you submitted value:' , value);
+     }
 }
