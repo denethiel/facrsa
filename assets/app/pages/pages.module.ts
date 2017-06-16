2cb2e8b1f32d511963ac95082dce834eb932c0a0
Jose David Pacheco Valedo <josepdark@gmail.com>
2017-06-16 18:55:21 -0500

    validation forms

--- assets/app/pages/pages.module.ts
+++ assets/app/pages/pages.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import {CommonModule} from '@angular/common';
 //import {FormsModule} from '@angular/forms';
 
 import { LoginComponent } from './login.component';
@@ -8,7 +9,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { PagesRoutingModule } from './pages-routing.module';
 
 @NgModule({
-    imports: [PagesRoutingModule, ReactiveFormsModule ],
+    imports: [PagesRoutingModule, ReactiveFormsModule, CommonModule ],
     declarations: [
         LoginComponent,
         RegisterComponent
--- assets/app/pages/register.component.ts
+++ assets/app/pages/register.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit} from '@angular/core';
 
 import { FormBuilder,
          FormGroup,
-         Validators} from '@angular/forms';
+         Validators, AbstractControl} from '@angular/forms';
 
 @Component({
     templateUrl:'app/pages/register.component.html'
@@ -10,14 +10,22 @@ import { FormBuilder,
 export class RegisterComponent{
 
     registerForm: FormGroup;
+
+    email: AbstractControl;
+    password: AbstractControl;
+    confirmPassword : AbstractControl;
+
     constructor(private fb: FormBuilder) { }
 
     createForm():void{
         this.registerForm = this.fb.group({
-            'email':[''],
-            'password':[''],
-            'confirmPassword':['']
+            'email':['', Validators.required],
+            'password':['', Validators.required],
+            'confirmPassword':['', Validators.required]
         });
+        this.email = this.registerForm.controls['email'];
+        this.password = this.registerForm.controls['password'];
+        this.confirmPassword = this.registerForm.controls['confirmPassword'];
     }
 
     ngOnInit(): void{
